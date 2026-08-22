93882702a2bc93a2c2419f7a416aee9a54830919
Victor Hugo Soares Castro <vhsoaresbr@gmail.com>
2026-08-22 14:37:56 -0300

    Add: Variáveis de ambiente c/ dotenv e subindo .env.example como instrução

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,13 +1,15 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 
+    const dialect = process.env.DB_DIALECT ?? "mysql"
+
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "12345",
-    database: "nodeapi",
+    type: dialect as "mysql" | "mariadb" | "postgres" | "mongodb" | "oracle",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+    username: process.env.DB_USERNAME,                 // Torna-se flexível -> o código já irá fazer automaticamente
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     synchronize: false,
     logging: true,
     entities: [],
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 // Importando a Biblioteca Express
 import express from "express";
 
+// Importar variáveis de ambiente
+import dotenv from "dotenv";
 
+// Carregando as variáveis do .env
+dotenv.config()
 
 // Criando Aplicação Express
 const app = express()
 
-
-
 // Incluir os controller
 import  login from "./controllers/login";
-
-
 
 // Criando as rotas
 app.use ('/', login)
@@ -19,7 +19,7 @@ app.use ('/', login)
 
 
 // Iniciar o servidor -> porta: 8080 (é uma porta padrão)
-app.listen(8080, () => {
-    console.log("Servidor iniciado na porta 8080: http://localhost:8080") 
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor iniciado na porta ${process.env.PORT}: http://localhost:${process.env.PORT}`) 
     
 });
