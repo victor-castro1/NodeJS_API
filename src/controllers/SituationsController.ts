32e8594f08d7b5c8246c5f75723a5a14f606dd1d
Victor Hugo Soares Castro <vhsoaresbr@gmail.com>
2026-09-13 14:05:04 -0300

    test: Adicionando rota GET e visualizar dados da conexão pelo Postman

--- src/controllers/SituationsController.ts
+++ src/controllers/SituationsController.ts
@@ -6,9 +6,51 @@ import { Situation } from "../entity/Situations";
 // Criando aplicação Express -> ROTAS
 const router = express();
 
-// Criar a rota GET - Principal
-router.get("/situations", (req:Request, res:Response) => {
-    res.send("Rota da situações está funcionando")
+// Criar a LISTA
+router.get("/situations", async(req:Request, res:Response) => {
+    try{
+
+        const situationRepository = AppDataSource.getRepository(Situation);
+        
+        const situations = await situationRepository.find();
+
+        res.status(200).json(situations);
+        return
+
+    } catch (error) {
+        res.status(500).json({
+            messagem : "Erro ao listar situação"
+        })
+        return;
+    }
+})
+
+// Criar a Visualização do item cadastrado em situação
+router.get("/situations/:id", async(req:Request, res:Response) => {
+    try{
+
+        const id = Number(req.params.id); // Converte um "texto" -> para um valor NUMÉRICO real
+
+        const situationRepository = AppDataSource.getRepository(Situation);
+        
+        const situation = await situationRepository.findOneBy({ id }) 
+
+        if (!situation) {
+            res.status(404).json({ 
+                messagem: "Situação não encontrada!",
+            });
+            return
+        }
+
+        res.status(200).json(situation);
+        return
+
+    } catch (error) {
+        res.status(500).json({
+            messagem : "Erro ao listar situação"
+        })
+        return
+    }
 })
 
 // Criar a rota POST 
